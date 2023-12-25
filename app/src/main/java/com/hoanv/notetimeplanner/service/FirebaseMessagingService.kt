06c4bbb30c428eea4daa5aae9c37b7a2c226125c
hoanv <hoanv@foxcode.org>
2023-12-25 17:31:35 +0700

    feat(Notification) add lib

---
+++ app/src/main/java/com/hoanv/notetimeplanner/service/FirebaseMessagingService.kt
@@ -0,0 +1,15 @@
+package com.hoanv.notetimeplanner.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class NoteTimePlannerService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
