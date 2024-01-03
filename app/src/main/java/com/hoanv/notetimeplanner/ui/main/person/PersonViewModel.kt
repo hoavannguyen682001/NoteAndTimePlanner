13f1eedd76594f59ab602377225db9f4cb705dc9
hoanv <hoanv@foxcode.org>
2024-01-03 16:11:45 +0700

    feat(person ui) add statistical person screen

--- app/src/main/java/com/hoanv/notetimeplanner/ui/main/person/PersonViewModel.kt
+++ app/src/main/java/com/hoanv/notetimeplanner/ui/main/person/PersonViewModel.kt
@@ -3,6 +3,9 @@ package com.hoanv.notetimeplanner.ui.main.person
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.hoanv.notetimeplanner.data.models.Category
+import com.hoanv.notetimeplanner.data.models.Task
 import com.hoanv.notetimeplanner.data.models.UserInfo
 import com.hoanv.notetimeplanner.data.repository.remote.RemoteRepo
 import com.hoanv.notetimeplanner.ui.base.BaseViewModel
@@ -16,9 +19,26 @@ import javax.inject.Inject
 class PersonViewModel @Inject constructor(
     private val remoteRepo: RemoteRepo
 ) : BaseViewModel() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val currentUser = auth.currentUser
+
     private val _userInfo = MutableLiveData<ResponseState<UserInfo>>()
     val userInfo: LiveData<ResponseState<UserInfo>>
         get() = _userInfo
+
+    private val _listCategories = MutableLiveData<List<Category>>()
+    val listCategories: LiveData<List<Category>>
+        get() = _listCategories
+
+    init {
+        if (currentUser != null) {
+            getUserInfo(currentUser.email!!)
+        }
+
+        getListCategory()
+    }
+
 
     fun getUserInfo(email: String) = viewModelScope.launch(Dispatchers.IO) {
         _userInfo.postValue(ResponseState.Start)
@@ -27,6 +47,16 @@ class PersonViewModel @Inject constructor(
                 _userInfo.postValue(ResponseState.Success(userInfo))
             } else {
                 _userInfo.postValue(ResponseState.Failure(Throwable("Fail")))
+            }
+        }
+    }
+
+    fun getListCategory() = viewModelScope.launch(Dispatchers.IO) {
+        remoteRepo.getListCategory() { list, state ->
+            if (state) {
+                _listCategories.postValue(list)
+            } else {
+                _listCategories.postValue(emptyList())
             }
         }
     }
--- app/src/main/java/com/hoanv/notetimeplanner/ui/main/person/activity/EditProfileVM.kt
+++ app/src/main/java/com/hoanv/notetimeplanner/ui/main/person/activity/EditProfileVM.kt
@@ -1,4 +1,9 @@
 package com.hoanv.notetimeplanner.ui.main.person.activity
 
-class EditProfileVM {
+import com.hoanv.notetimeplanner.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class EditProfileVM @Inject constructor() : BaseViewModel() {
 }
